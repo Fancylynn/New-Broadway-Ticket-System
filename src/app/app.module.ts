46f252c60a18e1033e1dff9bb9557950c5640956
Fancylynn <fancylynn0@gmail.com>
2018-06-23 18:51:05 -0400

    connect to the cloud firebase db to store order info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { ShowsComponent } from './components/shows/shows.component';
 import { ShowDetailComponent } from './components/show-detail/show-detail.component';
@@ -10,17 +8,16 @@ import { AppRoutingModule } from './/app-routing.module';
 import { TicketOrderComponent } from './components/ticket-order/ticket-order.component';
 import { LoginComponent } from './components/login/login.component';
 import { ShowService } from './services/show.service';
-
 import { MaterialModule } from './shared/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
-import { AngularFireModule } from 'angularfire2';
-
 // New imports to update based on AngularFire2 version 4
+import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import * as firebaseconfig from './firebase-config';
 
 export const firebaseConfig = {
@@ -52,7 +49,7 @@ export const firebaseConfig = {
     AngularFireAuthModule
   ],
   entryComponents: [ ShowDetailComponent, LoginComponent, SignUpComponent ],
-  providers: [ShowService, AngularFireAuth],
+  providers: [ShowService, AngularFireAuth, AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/ticket-order/ticket-order.component.ts
+++ src/app/components/ticket-order/ticket-order.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Order } from '../../models/order';
 import { Show } from '../../models/show';
 import { ShowService } from '../../services/show.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { MatDialog } from '@angular/material';
+import { LoginComponent } from '../login/login.component';
+
 @Component({
   selector: 'app-ticket-order',
   templateUrl: './ticket-order.component.html',
@@ -9,16 +16,48 @@ import { ShowService } from '../../services/show.service';
 })
 export class TicketOrderComponent implements OnInit {
 
-  constructor(private showService: ShowService) { }
+  constructor(private showService: ShowService, 
+              public af: AngularFireAuth,
+              private afs: AngularFirestore,
+              public dialog: MatDialog) { }
 
   shows: Show[];
+  currentOrder: Order = {
+    userid: '',
+    orderName: '',
+    phoneNumber: null,
+    email: '',
+    homeAddress: '',
+    showName: '',
+    showTime: '',
+    quantity: null,
+    delivery: '',
+    payment: ''
+  };
 
 
   ngOnInit() {
     this.getShows();
+    this.af.auth.onAuthStateChanged(user => {
+    if (user) {
+      this.currentOrder.userid = user.email;
+    }
+  });
   }
 
   getShows(): void {
     this.shows = this.showService.getShows();
+  }
+
+  onSubmitOrder() {
+    console.log(this.currentOrder);
+    if (this.currentOrder.userid.length > 0) {
+      this.afs.collection('order').add(this.currentOrder);
+    } else {
+      const dialogRef = this.dialog.open(LoginComponent, 
+      {
+        width: '500px'
+      });
+    }
   }
 }
